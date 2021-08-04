fbb7d6adc89ab1586d504f2d5958a9877c1da271
Sergey Gorshkov <gorshkovs372@gmail.com>
2021-08-04 22:50:50 +0300

    create megration seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,47 @@
 <?php
 
-namespace Database\Seeders;
-
+use Database\Seeders\AdminOrderProductsSeeder;
+use Database\Seeders\AttributeGroupsSeeder;
+use Database\Seeders\AttributeProductsSeeder;
+use Database\Seeders\AttributeValuesSeeder;
+use Database\Seeders\BrandsSeeder;
+use Database\Seeders\CategoriesSeeder;
+use Database\Seeders\CurrenciesSeeder;
+use Database\Seeders\GalleriesSeeder;
+use Database\Seeders\OrdersSeeder;
+use Database\Seeders\ProductsSeeder;
+use Database\Seeders\RelatedProductsSeeder;
+use Database\Seeders\RolesTableSeeder;
+use Database\Seeders\UserRolesTableSeeder;
+use Database\Seeders\UsersTableSeeder;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+
+    class DatabaseSeeder extends Seeder
     {
-        // \App\Models\User::factory(10)->create();
+        /**
+         * Seed the application's database.
+         *
+         * @return void
+         */
+        public function run()
+        {
+
+
+            $this->call(RolesTableSeeder::class);
+            $this->call(UsersTableSeeder::class);
+            $this->call(UserRolesTableSeeder::class);
+            $this->call(AttributeGroupsSeeder::class);
+            $this->call(AttributeProductsSeeder::class);
+            $this->call(AttributeValuesSeeder::class);
+            $this->call(BrandsSeeder::class);
+            $this->call(CategoriesSeeder::class);
+            $this->call(CurrenciesSeeder::class);
+            $this->call(GalleriesSeeder::class);
+            $this->call(ProductsSeeder::class);
+            $this->call(RelatedProductsSeeder::class);
+            $this->call(OrdersSeeder::class);
+            $this->call(AdminOrderProductsSeeder::class);
+
+        }
     }
-}
